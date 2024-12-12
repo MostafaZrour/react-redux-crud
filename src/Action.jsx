8cc93df4233b6048de7e500ea0c7144aee55796b
Zrour Mostfa <moustazro123@gmail.com>
2024-12-12 23:01:05 +0100

    delete setUser

--- src/Action.jsx
+++ src/Action.jsx
@@ -4,4 +4,3 @@
 export const addUser = (user) => ({ type: "ADD_USER", payload: user });
 export const updateUser = (user) => ({ type: "UPDATE_USER", payload: user });
 export const deleteUser = (userId) => ({ type: "DELETE_USER", payload: userId });
-export const setUsers = (users) => ({ type: "SET_USERS", payload: users });
